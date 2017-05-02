b7daf278e4a7afa62d8b09692a907cf8d82f9776
kieranfraser <kfraser@tcd.ie>
2017-05-02 10:33:39 +0100

    updated controller with cross origin request functionality

--- src/controllers/ResultController.java
+++ src/controllers/ResultController.java
@@ -2,12 +2,14 @@ package controllers;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import managers.NabsManager;
 
+@CrossOrigin(origins = "*", maxAge=3600)
 @RestController
 public class ResultController {
 
